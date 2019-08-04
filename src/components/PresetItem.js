065745562b1a5bf0a401b37840ab0febf1bd6764
MasonCable <masoncablelsc@gmail.com>
2019-08-04 07:30:36 -0700

    heroku push

--- src/components/PresetItem.js
+++ src/components/PresetItem.js
@@ -6,13 +6,14 @@ const apiKey = 'd5cb87e716a91af3a28e40e73bb74a37'
 
 class PresetItem extends React.Component {
     state = {
-        temp: ' '
+        temp: ' ',
+        windSpeeds: ''
     }
     componentDidMount() {
         axios.get(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?lat=${this.props.lat}&lon=${this.props.lon}&appid=${apiKey}&units=imperial`)
         .then(res => {
             this.setState({temp: res.data.main.temp})
-            console.log(this.state)
+            console.log(res.data)
         }).catch(err => (console.log(err)))
 
         
@@ -25,7 +26,9 @@ class PresetItem extends React.Component {
                     <img src={imgUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">Hello from {cityName}</h5>
-                        <p className="card-text"> <strong>Temperature</strong>: {this.state.temp} 	&#176; F</p>
+                        <p className="card-text"> <strong>Temperature</strong>: {this.state.temp} 	&#176; F</p> 
+                        <br/> 
+                        <p className="card-text"><strong>Coordinates</strong>: {lat}, {lon} </p>
                         
                 </div>
                 </div>
